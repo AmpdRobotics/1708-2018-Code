7afde0d33f9c7b2ef3930d98eb75f77b5c730886
Sarah Storck <soccerbot@gmail.com>
2018-02-19 18:15:01 -0500

    Simplified aitnomous code

--- src/org/usfirst/frc/team1708/robot/commands/AutoScoreSwitch.java
+++ src/org/usfirst/frc/team1708/robot/commands/AutoScoreSwitch.java
@@ -10,40 +10,35 @@ public class AutoScoreSwitch extends CommandGroup {
 		left, center, right
 	};
 
-	private double robotToSwitchDistance = 3;
+	private double robotToSwitchDistance = 3; //feet
+	private double oppositeSideDistance = 4; //feet
+	private double centerTimeoutSeconds = 2; //feet
+
 	private double centerToSwitchDistance = robotToSwitchDistance * .5;
-	private double oppositeSideDistance = 4;
 	private double centerToSideDistance = oppositeSideDistance * .5;
 
 	public AutoScoreSwitch(FieldPosition switch_position, FieldPosition robot_position) {
 		if (switch_position == robot_position) {
 			addSequential(new DriveForDistance(robotToSwitchDistance));
-		} else if (robot_position == FieldPosition.center) {
+		} 
+		else{
 			addSequential(new DriveForDistance(centerToSwitchDistance));
-			// drive straight
 			if (switch_position == FieldPosition.left) {
 				addSequential(new TurnToTheSpecifiedAngle(90));
 			} else if (switch_position == FieldPosition.right) {
 				addSequential(new TurnToTheSpecifiedAngle(-90));
-
 			}
-			addSequential(new DriveForDistance(centerToSideDistance));
-			addSequential(new TurnToTheSpecifiedAngle(0));
-			addSequential(new DriveForDistance(centerToSwitchDistance));
-		} else {
-			// drive straightaddSequential(new DriveForDistance(1.5));
-			// drive straight
-			if (switch_position == FieldPosition.left) {
-				addSequential(new TurnToTheSpecifiedAngle(90));
-
-			} else if (switch_position == FieldPosition.right) {
-				addSequential(new TurnToTheSpecifiedAngle(-90));
+			
+			if (robot_position == FieldPosition.center) {
+				addSequential(new DriveForDistance(centerToSideDistance));
 			}
-			addSequential(new DriveForDistance(oppositeSideDistance));
+			else {
+				addSequential(new DriveForDistance(oppositeSideDistance));
+			}
 			addSequential(new TurnToTheSpecifiedAngle(0));
 			addSequential(new DriveForDistance(centerToSwitchDistance));
 		}
-		addSequential(new CenterOnBlob());
+		addSequential(new CenterOnBlob(), centerTimeoutSeconds);
 		addSequential(new CubeDrop());
 	}
 }
--- src/org/usfirst/frc/team1708/robot/commands/DriveForDistance.java
+++ src/org/usfirst/frc/team1708/robot/commands/DriveForDistance.java
@@ -30,8 +30,7 @@ public class DriveForDistance extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.drivetrain.driveWithGyro(.75, 0);
-
+		Robot.drivetrain.drive(.75, 0);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
